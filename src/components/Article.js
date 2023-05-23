5616dd8cb819d1f87e6f9950cbfe9b4a868bcc95
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-05-22 23:58:50 -0400

    Jest test requirements fulfilled, add minutes to articles

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function Article({title, date="January 1, 1970", preview}) {
+function Article({title, date="January 1, 1970", preview, minutes}) {
     return (
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
             <p>{preview}</p>
+            <small>{minutes} minutes</small>
         </article>
     )
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,9 +4,15 @@ import Article from './Article'
 function ArticleList({posts}) {
   return (
     <main>
-        <Article key={posts[0].id} post={posts[0]}/>
-        <Article key={posts[1].id} post={posts[1]}/>
-        <Article key={posts[2].id} post={posts[2]}/>
+        {posts.map((post) => (
+            <Article 
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+            minutes={post.minutes}
+            />
+        ))}
     </main>
   )
 }
